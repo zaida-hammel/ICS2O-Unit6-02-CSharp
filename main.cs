6bbe3c76de0e3f77141b688cb138d4f0ee5a5749
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-05-18 20:36:19 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,31 @@
+// Created by: Zaida Hammel
+// Created on: May 2020
+// This file contains the Csharp code
+
+using System;
+using System.IO;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        if (File.Exists("text.txt") == false)
+        {
+            File.WriteAllText("text.txt", "0");
+        }
+        string fileText = File.ReadAllText("text.txt");
+        fileText = Convert.ToString(Convert.ToInt32(fileText) + 1);
+        File.WriteAllText("text.txt", fileText);
+        if (Convert.ToInt32(fileText) == 1)
+        {
+            Console.WriteLine("You’ve opened this program 1 time.");
+        }
+        else
+        {
+            Console.WriteLine("You’ve opened this program " + fileText + " times.");
+        }
+        Console.WriteLine("\nPress any key to quit...");
+        Console.ReadKey();
+        Console.WriteLine("\nFinished.");
+    }
+}
